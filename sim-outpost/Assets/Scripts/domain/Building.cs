db81f8b9625bc11172d56d34241f0749d0c42320
skanaar <daniel.kallin@gmail.com>
2018-01-27 16:21:56 +0100

    moving stuff around

---
+++ sim-outpost/Assets/Scripts/domain/Building.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class Building {
+    public BuildingType type;
+    public int i;
+    public int j;
+    public float power;
+    public bool enabled;
+    public GameObject gameObject;
+}
+
+public class BuildingType {
+    public string name;
+    public float height;
+    public int w;
+    public int h;
+    public Attr turnover;
+    public string model;
+}
+
